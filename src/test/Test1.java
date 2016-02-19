424998e9e5e1203a071a260378cb853568563661
Misha Seltzer <misha+git@seltzer.com>
2016-02-19 18:10:25 +0200

    First implementation of auto threader's future creation

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -3,25 +3,17 @@ package test;
 import java.util.Random;
 
 public class Test1 {
-	
 	public final static void main(String[] args) throws InterruptedException {
 		long start = System.currentTimeMillis();
 
-		int a = heavyFunc();
-		/*
-		Future<Integer> a = Threader.thread(new Callable<Integer>() {
-			@Override
-			public Integer call() throws Exception {
-				return heavyFunc();
-			}
-		});
-		*/ 
-		int b = heavyFunc();
-		System.out.println((a+b) + " " + (System.currentTimeMillis() - start));
-		// System.out.println((a.get()+b) + " " + (System.currentTimeMillis() - start));
+		Integer a = heavyFunc();
+		Integer b = heavyFunc();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(System.currentTimeMillis() - start);
 	}
 	
-	public static int heavyFunc() throws InterruptedException {
+	public static Integer heavyFunc() throws InterruptedException {
 		Thread.sleep(1000);
 		return new Random().nextInt();
 	}
