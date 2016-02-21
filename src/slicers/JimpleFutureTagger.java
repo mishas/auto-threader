5b64dcdc25d2ff93f4bfe3d4084be614994a0f3a
ron-lenovo <ron-lenovo@192.168.1.7>
2016-02-21 19:09:42 +0200

    cosmetics

--- src/slicers/JimpleFutureTagger.java
+++ src/slicers/JimpleFutureTagger.java
@@ -104,8 +104,8 @@ public class JimpleFutureTagger {
 						Set<Unit> defUnits = localsOfInterest.get(l);
 						for(Unit defUnit:defUnits){
 							//for cases like def before conditional that defines all
-							if(defInAllPaths(defUnit,u,l,localsOfInterest))
-								localsOfInterest.get(l).remove(defUnit);
+							//if(defInAllPaths(defUnit,u,l,localsOfInterest))
+								//localsOfInterest.get(l).remove(defUnit);
 							
 							if(!defUnit.hasTag(DependentsTag.name))
 								defUnit.addTag(new DependentsTag(useVal, useVal.getType()));
@@ -134,95 +134,4 @@ public class JimpleFutureTagger {
 		}
 		return false;
 	}
-	
-	private boolean defInAllPaths(Unit defUnit, Unit u, Local l,Map<Local,Set<Unit>> localsOfInterest) {
-		List<Unit> path;
-		for(Unit succ:g.getSuccsOf(defUnit)){
-			path=null;//TODO no good. find all paths, really. = g.getExtendedBasicBlockPathBetween(succ,u);
-			if(!isDefInPath(path,l))
-				return false;
-		}			
-		return true;
-	}
-
-	private boolean isDefInPath(List<Unit> path, Local l) {
-		if(path==null) return false;
-		for(Unit u:path){
-			List<ValueBox> defBoxes = u.getDefBoxes();
-			for(ValueBox vb:defBoxes)
-				if(vb.getValue() == l)
-					return true;
-		}
-		return false;		
-	}
-
-	/*
-	private void tag(PatchingChain<Unit> pc) {
-		List<ValueBox> defBoxes;// all defined locals in a unit
-		List<ValueBox> usedBoxes;// all used locals in a unit
-		Set<ValueBox> toSkip;// locals that were defined again later on
-		List<ValueBox> redefBoxes;// locals defined in defBoxes but also later
-									// on
-		Set<Local> usedLocals;
-		DependentsTag dt;
-
-		for (Unit defUnit : pc) {
-			dt = new DependentsTag();
-			defUnit.addTag(dt);
-			defBoxes = defUnit.getDefBoxes();
-			toSkip = new HashSet<ValueBox>();
-
-			for (Unit u : this.b.getUnits()) {
-				if (before(u, defUnit))
-					continue;// look only ahead..
-
-				// first ignore all locals that are defined here (if not same
-				// line)
-				if (u != defUnit) {
-					redefBoxes = u.getDefBoxes();
-					for (ValueBox vb : redefBoxes)
-						for (ValueBox db : defBoxes)
-							if (db.getValue() == vb.getValue()) {
-								System.out
-										.println(db.getValue() + " from " + defUnit + " was added to skip after " + u);
-								toSkip.add(db);
-							}
-				}
-
-				usedLocals = new HashSet<Local>();
-				usedBoxes = u.getUseBoxes();
-
-				for (ValueBox usedBox : usedBoxes) {
-					Object l = usedBox.getValue();
-					if (l instanceof Local)
-						usedLocals.add((Local) l);
-				}
-				for (Local l : usedLocals) {
-					for (ValueBox vb : defBoxes) {
-						if (toSkip.contains(vb)) {
-							continue;
-						}
-						if (vb.getValue() == l)
-							dt.addDependent(vb, u);
-					}
-				}
-			}
-			/*
-			 * if(dt.getDependents().keySet().size()==0) continue;
-			 * System.out.println("Unit: "+defUnit+" line "
-			 * +defUnit.getJavaSourceStartLineNumber()+" \nDependents: ");
-			 * for(ValueBox vb:dt.getDependents().keySet()){ System.out.println(
-			 * "\tLocal: "+vb.getValue().toString()); for(Unit
-			 * dep:dt.getDependents().get(vb)) System.out.println("\t\t\t"+dep);
-			 * }
-			 
-		}
-	}
-
-	private boolean before(Unit u1, Unit u2) {
-		return u1.getJavaSourceStartLineNumber() == u2.getJavaSourceStartColumnNumber()
-				? u1.getJavaSourceStartColumnNumber() < u2.getJavaSourceStartColumnNumber()
-				: u1.getJavaSourceStartLineNumber() < u2.getJavaSourceStartLineNumber();
-	}
-	*/
 }
